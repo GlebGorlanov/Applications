d82699d7ba0cd0d8fc3d5ca4f92a8cd78ef15ca3
GlebGorlanov <gorlanovgleb@gmail.com>
2025-11-11 19:56:37 +0300

    Update main.cjs

--- electron/main.cjs
+++ electron/main.cjs
@@ -16,13 +16,25 @@ ipcMain.on('window-close', () => {
 });
 
 function createWindow() {
+  let iconPath;
+  switch (process.platform) {
+    case 'win32':
+      iconPath = path.join(__dirname, '../build/icon.ico');
+      break;
+    case 'darwin':
+      iconPath = path.join(__dirname, '../build/icon.icns');
+      break;
+    default:
+      iconPath = path.join(__dirname, '../build/icons/512x512.png');
+  }
+
   mainWindow = new BrowserWindow({
     frame: false,
     transparent: true,
     hasShadow: false,
     useContentSize: true,
     backgroundColor: '#00000000',
-    icon: path.join(__dirname, '../public/icon.ico'),
+    icon: iconPath,
 
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
@@ -30,54 +42,42 @@ function createWindow() {
       contextIsolation: true,
     },
   });
-  
+
   Menu.setApplicationMenu(null);
 
   mainWindow.setSize(400, 630);
-  mainWindow.setMinimumSize(300, 475); /*min size*/
-  mainWindow.setMaximumSize(460, 745); /*max size*/
+  mainWindow.setMinimumSize(300, 475);
+  mainWindow.setMaximumSize(460, 745);
 
   if (process.env.VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(process.env.VITE_DEV_SERVER_URL);
     mainWindow.webContents.openDevTools();
   } else {
     mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
-  };
+  }
 
   mainWindow.on('resize', () => {
     const [winWidth, winHeight] = mainWindow.getSize();
-  
-    // Установи свой min и max размеры
+
     const minHeight = 475;
     const maxHeight = 745;
-  
     const minRatio = 1 / 1.608;
     const maxRatio = 1 / 1.555;
 
-    // Ограничим высоту в рамках [minHeight, maxHeight]
     const clampedHeight = Math.max(minHeight, Math.min(maxHeight, winHeight));
-  
-    // Преобразуем высоту в [0,1] диапазон
     const t = (clampedHeight - minHeight) / (maxHeight - minHeight);
-  
-    // Линейная интерполяция между minRatio и maxRatio
     const currentAspect = minRatio + (maxRatio - minRatio) * t;
-  
+
     mainWindow.setAspectRatio(currentAspect);
-  });  
-};
+  });
+}
 
 app.whenReady().then(createWindow);
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+  if (process.platform !== 'darwin') app.quit();
 });
 
 app.on('activate', () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
 });
-
